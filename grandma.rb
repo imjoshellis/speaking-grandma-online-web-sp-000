da2a4a05170e49d50fb4ba733a41461f87a51b93
Josh Ellis <joshe523@gmail.com>
2020-03-02 21:03:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN"
+      return "I LOVE YOU TOO PUMPKIN!"
 
     else
-      return " NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
 
     end
   else
